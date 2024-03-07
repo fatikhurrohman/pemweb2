79a6ebd89f296fd487c928d15c9d3c905f779d7f
fatikhurrohman~ <fatikhurrohman29@.com>
2024-03-07 23:23:14 +0700

    editing routing dan controller

---
+++ pertemuan3/yoi/app/Controllers/page.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controllers;
+
+class page extends BaseController
+{
+    public function about()
+    {
+        echo "about page";
+    }
+
+    public function contact()
+    {
+        echo "contact page";
+    }
+
+    public function faqs()
+    {
+        echo "faqs page";
+    }
+
+    public function tos()
+    {
+        echo "Halaman Term of Service";
+    }
+
+    public function fatik()
+    {
+        echo "NAMA        :fatikhurrohman";
+        echo "<br> NIM        :4122042";
+        echo "<br>ALAMAT      :klampisan";
+    }
+}
