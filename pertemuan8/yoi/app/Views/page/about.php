3dea0609890d9492440c2cecee38b7caab665938
abdhalim18 <fatikhurrohman29@gmail.com>
2024-06-03 01:34:58 +0700

    validation and delete data in ci

---
+++ pertemuan8/yoi/app/Views/page/about.php
@@ -0,0 +1,12 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="rows">
+        <div class="col">
+            <h1>tentang seorang remaja dengan segala pikirnya</h1>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
+    
